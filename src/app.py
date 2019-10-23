b8b16a3e4d03448f269194da95a1d9f00876487a
Michael Götz <michael-guenther.goetz@stud.uni-bamberg.de>
2019-10-24 00:02:22 +0200

    Implement api docs, Close #3

--- src/app.py
+++ src/app.py
@@ -1,18 +1,18 @@
 import os
-import time
-from pprint import pprint
-from urllib.parse import urlencode, quote_plus
 
 import requests
-from flask import Flask, request, jsonify
-from flask_caching import Cache
+from flask import Flask, jsonify
+from flask_restplus import Api, Resource, reqparse, inputs
+
+import utils
 
 UNIVIS_ROOMS_API = os.environ.get("UNIVIS_ROOM_API")
 UNIVIS_ALLOCATION_API = os.environ.get("UNIVIS_ALLOCATION_API")
 API_V1_ROOT = "/api/v1/"
 
 app = Flask(__name__)
-cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+api = Api(app=app, doc='/docs', version='1.0', title='RooFiS2 API',
+          description='Room Finder Service 2 API')
 
 BUILDING_KEY_MAP = {"Erba": {'building_keys': ['WE5'], 'geojson_file': ''},
                     "Feki": {'building_keys': ["F21", "FG1", "FG2", "FMA"], 'geojson_file': ''},
@@ -32,130 +32,73 @@ EXCLUDED_ROOM_NAMES = ["Tower Lounge WIAI", "PC-Labor", "PC-Labor 1", "PC-Labor 
                        "Lernwerkstatt", "Sitzungszimmer Fakultät GuK", "Lehrredaktion", "Arbeits-, und Materialraum",
                        "Sitzungszimmer Fakultät GuK"]
 
-
-@app.route(f'{API_V1_ROOT}/', methods=['GET'])
-def roofis():
-    start_date = request.args.get('start_date', None)
-    start_time = request.args.get('start_time', None)
-    min_size = request.args.get('min_size', None)
-    location = request.args.get('location', None)
-    building_key = request.args.get('building_key', None)
-
-    if start_date and start_time:
-        allocations_url = _get_allocation_url(start_date=start_date, start_time=start_time)
-        if not location:
-            rooms_url = _get_rooms_url(building_keys=[building_key] if building_key else None)
-        elif location in BUILDING_KEY_MAP:
-            rooms_url = _get_rooms_url(building_keys=BUILDING_KEY_MAP[location]['building_keys'])
-        else:
-            return jsonify(status_code=400)
-
-        r_allocations = requests.get(allocations_url)
-        r_rooms = requests.get(rooms_url)
-
-        if r_allocations.status_code == 200 and r_rooms.status_code == 200:
-            allocations = r_allocations.json()
-
-            rooms = r_rooms.json()
-            allocated_rooms = get_allocated_rooms(allocations)
-
-            rooms = [add_allocations(room, allocated_rooms) for room in rooms]
-
-            free_rooms = [add_allocations(room, allocated_rooms) for room in rooms if
-                          not is_currently_allocated(room, start_time) and is_excluded(room, min_size)]
-            for room in free_rooms:
-                add_next_allocation(room, start_time)
-
-            free_rooms.sort(key=lambda room: f'{room["building_key"]}/{room["floor"]:02}.{room["number"]:03}')
-            return jsonify(free_rooms)
-    return jsonify(status_code=400)
+roofis_parser = reqparse.RequestParser()
+roofis_parser.add_argument('start_date', required=True,
+                           type=inputs.regex('^([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))$'),
+                           help='date from which free rooms are to be searched for')
+roofis_parser.add_argument('start_time', required=True,
+                           type=inputs.regex('^(?:[01]\d|2[0123]):(?:[012345]\d)$'),
+                           help='time from which free rooms are to be searched for')
+roofis_parser.add_argument('min_size', type=int, help='filter room by minumum room size')
+roofis_parser.add_argument('location', type=str, help='filter rooms by location')
+roofis_parser.add_argument('building_key', type=str, help='filter rooms by building key')
 
 
-def get_allocated_rooms(allocations):
-    allocated_rooms = {}
-    for allocation in allocations:
-        for room in allocation['rooms']:
-            if room['univis_key'] not in allocated_rooms or 'allocations' not in allocated_rooms[
-                room['univis_key']]:
-                allocated_rooms[room['univis_key']] = {}
-                allocated_rooms[room['univis_key']]['allocations'] = [allocation]
+@api.route(f'{API_V1_ROOT}')
+class Roofis(Resource):
+
+    @api.doc(parser=roofis_parser)
+    def get(self):
+        """
+        returns a list of rooms that are free
+        """
+
+        args = roofis_parser.parse_args()
+
+        start_date = args.get('start_date', None)
+        start_time = args.get('start_time', None)
+        min_size = args.get('min_size', None)
+        location = args.get('location', None)
+        building_key = args.get('building_key', None)
+
+        if start_date and start_time:
+            allocations_url = utils._get_allocation_url(start_date=start_date, start_time=start_time)
+            if not location:
+                rooms_url = utils._get_rooms_url(building_keys=[building_key] if building_key else None)
+            elif location in BUILDING_KEY_MAP:
+                rooms_url = utils._get_rooms_url(building_keys=BUILDING_KEY_MAP[location]['building_keys'])
             else:
-                allocated_rooms[room['univis_key']]['allocations'].append(allocation)
-    return allocated_rooms
+                return jsonify(status_code=400)
+
+            r_allocations = requests.get(allocations_url)
+            r_rooms = requests.get(rooms_url)
+
+            if r_allocations.status_code == 200 and r_rooms.status_code == 200:
+                allocations = r_allocations.json()
+
+                rooms = r_rooms.json()
+                allocated_rooms = utils.get_allocated_rooms(allocations)
+
+                rooms = [utils.add_allocations(room, allocated_rooms) for room in rooms]
+
+                free_rooms = [utils.add_allocations(room, allocated_rooms) for room in rooms if
+                              not utils.is_currently_allocated(room, start_time) and utils.is_excluded(room, min_size)]
+                for room in free_rooms:
+                    utils.add_next_allocation(room, start_time)
+
+                free_rooms.sort(key=lambda room: f'{room["building_key"]}/{room["floor"]:02}.{room["number"]:03}')
+                return jsonify(free_rooms)
+        return jsonify(status_code=400)
 
 
-def add_allocations(room, allocations):
-    room['allocations'] = allocations[room['univis_key']]['allocations'] if room['univis_key'] in allocations else []
-    return room
-
-
-def is_excluded(room, min_size):
-    if min_size:
-        return room["orgname"] not in EXCLUDED_ORGNAMES and room["name"] not in EXCLUDED_ROOM_NAMES and room["name"] and \
-               room['size'] >= int(min_size)
-    return room["orgname"] not in EXCLUDED_ORGNAMES and room["name"] not in EXCLUDED_ROOM_NAMES and room["name"]
-
-
-def is_currently_allocated(room, start_time):
-    requested_start_time = time.strptime(start_time, "%H:%S")
-    for allocation in room['allocations']:
-        start_time = time.strptime(allocation['start_time'], "%H:%S")
-        end_time = time.strptime(allocation['end_time'], "%H:%S")
-        if start_time.tm_hour <= requested_start_time.tm_hour < end_time.tm_hour:
-            return True
-    return False
-
-
-def add_next_allocation(room, start_time):
-    requested_start_time = time.strptime(start_time, "%H:%S")
-    for allocation in room['allocations']:
-        start_time = time.strptime(allocation['start_time'], "%H:%S")
-        if requested_start_time.tm_hour < start_time.tm_hour:
-            if 'next_allocation' in room:
-                current_next_allocation = time.strptime(room['next_allocation'], "%H:%S")
-                if current_next_allocation.tm_hour > start_time.tm_hour:
-                    room['next_allocation'] = time.strftime("%H:%S", start_time)
-            else:
-                room['next_allocation'] = time.strftime("%H:%S", start_time)
-    return None
-
-
-def _get_allocation_url(building_key=None, end_date=None, end_time=None, faculty=None, start_date=None,
-                        start_time=None):
-    params = {}
-    params['start_date'] = start_date
-    params['end_date'] = end_date if end_date else start_date
-    params['start_time'] = start_time
-    if faculty:
-        params['department'] = faculty
-    if building_key:
-        params['name'] = building_key
-    return f'{UNIVIS_ALLOCATION_API}?{urlencode(params, quote_via=quote_plus)}'
-
-
-def _get_rooms_url(building_keys=None, faculty=None):
-    params = {}
-    if faculty:
-        params['department'] = faculty
-    if building_keys:
-        params['building_keys'] = building_keys
-    return f'{UNIVIS_ROOMS_API}?{urlencode(params, True, quote_via=quote_plus)}'
-
-
-@app.route(f'{API_V1_ROOT}/building_keys/', methods=['GET'])
-def building_keys():
-    return jsonify(status_code=400)
-
-
-@app.route(f'{API_V1_ROOT}/faculties/', methods=['GET'])
-def faculties():
-    return jsonify(status_code=400)
-
-
-@app.route(f'{API_V1_ROOT}locations/', methods=['GET'])
-def locations():
-    locations = [location for location in BUILDING_KEY_MAP]
-    return jsonify(locations)
+@api.route(f'{API_V1_ROOT}locations/')
+class LocationList(Resource):
+    def get(self):
+        """
+        returns list of available locations
+        """
+        locations = [location for location in BUILDING_KEY_MAP]
+        return jsonify(locations)
 
 
 if __name__ == '__main__':
---
+++ src/utils.py
@@ -0,0 +1,75 @@
+import time
+from urllib.parse import urlencode, quote_plus
+
+from src.app import EXCLUDED_ORGNAMES, EXCLUDED_ROOM_NAMES, UNIVIS_ALLOCATION_API, UNIVIS_ROOMS_API
+
+
+def get_allocated_rooms(allocations):
+    allocated_rooms = {}
+    for allocation in allocations:
+        for room in allocation['rooms']:
+            if room['univis_key'] not in allocated_rooms or 'allocations' not in allocated_rooms[
+                room['univis_key']]:
+                allocated_rooms[room['univis_key']] = {}
+                allocated_rooms[room['univis_key']]['allocations'] = [allocation]
+            else:
+                allocated_rooms[room['univis_key']]['allocations'].append(allocation)
+    return allocated_rooms
+
+
+def add_allocations(room, allocations):
+    room['allocations'] = allocations[room['univis_key']]['allocations'] if room['univis_key'] in allocations else []
+    return room
+
+
+def is_excluded(room, min_size):
+    if min_size:
+        return room["orgname"] not in EXCLUDED_ORGNAMES and room["name"] not in EXCLUDED_ROOM_NAMES and room["name"] and \
+               room['size'] >= int(min_size)
+    return room["orgname"] not in EXCLUDED_ORGNAMES and room["name"] not in EXCLUDED_ROOM_NAMES and room["name"]
+
+
+def is_currently_allocated(room, start_time):
+    requested_start_time = time.strptime(start_time, "%H:%S")
+    for allocation in room['allocations']:
+        start_time = time.strptime(allocation['start_time'], "%H:%S")
+        end_time = time.strptime(allocation['end_time'], "%H:%S")
+        if start_time.tm_hour <= requested_start_time.tm_hour < end_time.tm_hour:
+            return True
+    return False
+
+
+def add_next_allocation(room, start_time):
+    requested_start_time = time.strptime(start_time, "%H:%S")
+    for allocation in room['allocations']:
+        start_time = time.strptime(allocation['start_time'], "%H:%S")
+        if requested_start_time.tm_hour < start_time.tm_hour:
+            if 'next_allocation' in room:
+                current_next_allocation = time.strptime(room['next_allocation'], "%H:%S")
+                if current_next_allocation.tm_hour > start_time.tm_hour:
+                    room['next_allocation'] = time.strftime("%H:%S", start_time)
+            else:
+                room['next_allocation'] = time.strftime("%H:%S", start_time)
+    return None
+
+
+def _get_allocation_url(building_key=None, end_date=None, end_time=None, faculty=None, start_date=None,
+                        start_time=None):
+    params = {}
+    params['start_date'] = start_date
+    params['end_date'] = end_date if end_date else start_date
+    params['start_time'] = start_time
+    if faculty:
+        params['department'] = faculty
+    if building_key:
+        params['name'] = building_key
+    return f'{UNIVIS_ALLOCATION_API}?{urlencode(params, quote_via=quote_plus)}'
+
+
+def _get_rooms_url(building_keys=None, faculty=None):
+    params = {}
+    if faculty:
+        params['department'] = faculty
+    if building_keys:
+        params['building_keys'] = building_keys
+    return f'{UNIVIS_ROOMS_API}?{urlencode(params, True, quote_via=quote_plus)}'
